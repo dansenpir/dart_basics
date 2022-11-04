d11aa9bdbf970dcbcd57c70217245337668ee4d1
Dan <dansenpirdev@gmail.com>
2022-11-04 09:10:24 -0300

    feat: data types

---
+++ 03_data_types.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  // primitive data types
+  bool boolOutput = false;
+  print(boolOutput);
+
+  int intOutput = 1;
+  print(intOutput);
+
+  double doubleOutput = 1.5;
+  print(doubleOutput);
+
+  // complex data types
+  String stringOutput = 'text';
+  print(stringOutput);
+
+  dynamic dynamicVariable = 4.5;
+  dynamicVariable = 'text';
+  print(dynamicVariable);
+
+  // keywords
+  late int exampleInt;
+  exampleInt = 1;
+  print(exampleInt);
+
+  final int finalInt = 2;
+  print(finalInt);
+
+  const int constInt = 4;
+  print(constInt);
+
+  var varOutput = "text";
+}
