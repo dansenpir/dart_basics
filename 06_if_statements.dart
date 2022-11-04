159f5d4a130822d97bd9eb627c7602f588365e8b
Dan <dansenpirdev@gmail.com>
2022-11-04 20:04:09 -0300

    feat: if statements

---
+++ 06_if_statements.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int age = 16;
+
+  print('user is $age years old.');
+
+  if (age >= 18) {
+    print('You can enter the movie.');
+  } else if (age >= 16) {
+    print('You can enter the movie with your parents.');
+  } else {
+    print('You cannot enter the movie.');
+  }
+}
