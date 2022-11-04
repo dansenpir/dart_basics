84fbc97b03674b27431983abce7b06eb36b41a2d
Dan <dansenpirdev@gmail.com>
2022-11-04 20:33:42 -0300

    feat: switch case

---
+++ 07_switch_case.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  final String name = "Peter";
+
+  switch (name) {
+    case "Hans":
+      print('my name is Hans');
+      break;
+    case "Peter":
+      print('my name is Peter.');
+      break;
+    default:
+      print('I dont know my name');
+  }
+}
