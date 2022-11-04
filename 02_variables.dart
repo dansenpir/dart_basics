9d1710915dc144e56f2100f20e59a820bd269b77
Dan <dansenpirdev@gmail.com>
2022-11-04 08:10:11 -0300

    feat: gets to know variables

---
+++ 02_variables.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  int outputInt = 2; // initialization
+  print('outputInt: $outputInt');
+
+  late int lateOutputInt; // declaration
+
+  lateOutputInt = 42; //allocation
+  print('lateOutputInt: $lateOutputInt');
+}
