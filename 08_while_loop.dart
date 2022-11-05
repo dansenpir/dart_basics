8b67992b6b7b86029ab4b934086c56bbd1f41477
Dan <dansenpirdev@gmail.com>
2022-11-05 10:18:39 -0300

    feat: while loop

---
+++ 08_while_loop.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int number = 1;
+
+  while (number < 100) {
+    print('current number: $number');
+    number += 2;
+  }
+
+  print('number after while: $number');
+}
