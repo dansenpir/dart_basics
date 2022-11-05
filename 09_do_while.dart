e5c7116c146b412abe3853781e7586133d625b28
Dan <dansenpirdev@gmail.com>
2022-11-05 14:39:26 -0300

    feat: do while

---
+++ 09_do_while.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  int number = 1;
+
+  do {
+    print('current number: $number');
+    number++;
+  } while (number < 1);
+
+  print('number after do while: $number');
+}
