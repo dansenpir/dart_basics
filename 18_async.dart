1a09cd1bc9d833682b1a815aa5a75e1c8b3f2c35
Dan <dansenpirdev@gmail.com>
2022-11-09 11:28:58 -0300

    feat: async

---
+++ 18_async.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) async {
+  DataService dataService = DataService();
+  String data = await dataService.getData();
+  print(data);
+}
+
+class DataService {
+  Future<String> _getDataFromCloud() async {
+    await Future.delayed(Duration(seconds: 4));
+    print('get data finished');
+    return 'fake data';
+  }
+
+  Future<String> _parseDataFromCloud({required String dataFromCloud}) async {
+    await Future.delayed(Duration(seconds: 2));
+    print('parse data finished');
+    return 'parsed data';
+  }
+
+  Future<String> getData() async {
+    // final String dataFromCloud = await _getDataFromCloud();
+    // final String parsedData =
+    //     await _parseDataFromCloud(dataFromCloud: dataFromCloud);
+
+    // ! alternative way syntax
+
+    final String parsedData =
+        await _getDataFromCloud().then((dataFromCloud) async {
+      return await _parseDataFromCloud(dataFromCloud: dataFromCloud);
+    });
+
+    return parsedData;
+  }
+}
